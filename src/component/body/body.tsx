3b1701a3c81e062074f105cd025e24505dcc7c93
kkt9102 <kkt9102@gmail.com>
2024-04-09 15:44:58 +0900

    project first setting | add recoil, .prettierrc

---
+++ src/component/body/body.tsx
@@ -0,0 +1,9 @@
+"use client";
+// MODULE
+
+// RECOIL STATE
+// COMPONENT
+
+export default function Body({ children }: { children: React.ReactNode }) {
+  return <div className={``}>{children}</div>;
+}
---
+++ src/util/recoilRootProvider.tsx
@@ -0,0 +1,17 @@
+// recoilRootProvider.tsx
+"use client";
+import Body from "@component/body";
+// MODULE
+import { RecoilRoot } from "recoil";
+
+export default function RecoilRootProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <RecoilRoot>
+      <Body>{children}</Body>
+    </RecoilRoot>
+  );
+}
